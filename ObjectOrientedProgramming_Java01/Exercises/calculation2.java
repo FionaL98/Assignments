a728974cea699481c51aabc9a1eacab799ad02d4
Fiona L <62258401+FionaL98@users.noreply.github.com>
2022-02-22 20:29:03 -0500

    Delete ObjectOrientedProgramming_Java01 directory

--- ObjectOrientedProgramming_Java01/Exercises/calculation2.java
+++
@@ -1,9 +0,0 @@
-public class calculation2 {
-	
-	public static void main(String[] args){
-		
-		System.out.println("The result of 100+200 is     which is a positive number.");
-		System.out.println("The result fo 100-200 is     whcih is a negative number.");
-		
-	}
-}
